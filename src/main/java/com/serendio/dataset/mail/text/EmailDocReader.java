fa04bb638ff0e9660b4d7c978156fb774b7d29f2
Michael Hunger <github@jexp.de>
2016-07-23 23:45:29 +0200

    fixed message content and from errors

--- src/main/java/com/serendio/dataset/mail/text/EmailDocReader.java
+++ src/main/java/com/serendio/dataset/mail/text/EmailDocReader.java
@@ -3,11 +3,9 @@ package com.serendio.dataset.mail.text;
 import com.serendio.Utils.Utils;
 import com.serendio.dataset.domain.EmailDoc;
 
-import javax.mail.Header;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.internet.MimeMessage;
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.Scanner;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -40,11 +38,12 @@ public class EmailDocReader {
 
 		EmailDoc mailDoc = new EmailDoc();
 		mailDoc.setName(getNameFromHeader(email));
-		mailDoc.setFrom(email.getFrom()[0] + "");
+		mailDoc.setFrom(getFrom(email));
 		mailDoc.setSubject(email.getSubject());
 		mailDoc.setMessage_ID(email.getMessageID());
 		mailDoc.setDate(email.getSentDate().toString());
-		mailDoc.setContent(email.getContent().toString().trim());
+		System.err.println(getContent(email));
+		mailDoc.setContent(getContent(email));
 		mailDoc.setTo(Utils.addressArrayToHashset(email
 				.getRecipients(Message.RecipientType.TO)));
 		mailDoc.setBcc(Utils.addressArrayToHashset(email
@@ -53,7 +52,38 @@ public class EmailDocReader {
 				.getRecipients(Message.RecipientType.CC)));
 		return mailDoc;
 	}
+	
+	private String getContent(MimeMessage email) {
+		try {
+		Object content = email.getContent();
+		if (content instanceof String) return content.toString().trim();
+		if (content instanceof Multipart) {
+            Part messagePart = ((Multipart) content).getBodyPart(0);
+            String contentType = messagePart.getContentType().toUpperCase();
+            if (contentType.startsWith("TEXT/PLAIN") || contentType.startsWith("TEXT/HTML")) {
+				Scanner scanner = new Scanner(messagePart.getInputStream()).useDelimiter("\\Z");
+				String result = scanner.next();
+				return result.trim(); // todo replace html
+          	}
+        }
+        } catch(Exception e) {
+	      throw new RuntimeException("Error getting content from email "+email,e);
+        }
+		return null;
+	}
 
+	private String getFrom(MimeMessage email) {
+        try {
+		   return email.getFrom()[0].toString();
+	    } catch(javax.mail.internet.AddressException ae) {
+           ae.printStackTrace();
+           String msg = ae.getMessage();
+		   if (msg.contains("``")) {
+		       return msg.substring(msg.indexOf("``")+2,msg.indexOf("''"));
+		   }
+	    }
+		return null;
+	}
 	private String getNameFromHeader(MimeMessage email)
 			throws MessagingException {
 		// TODO Auto-generated method stub
